c4840923e602c05000d1b70ece0793a918e9b508
tashana1999 <85959669+tashana1999@users.noreply.github.com>
2021-09-04 17:57:35 +0530

    Add files via upload

---
+++ 3.5.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include <iostream> 
+#include <math.h>
+int main()
+{
+	
+	cout << "ppppppppp"<<endl;
+	cout << "    p   p"<<endl;
+	cout << "    p   p"<<endl;
+	cout << "    p   p"<<endl;
+	cout << "     p p "<<endl;
+	cout << "  JJ      " <<endl;
+	return 0;
+  }  
