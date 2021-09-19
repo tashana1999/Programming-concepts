76a5fc8119dc5752580decdac9e4f229d0d432ec
tashana1999 <85959669+tashana1999@users.noreply.github.com>
2021-09-19 09:00:47 +0530

    Add files via upload

---
+++ startype6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=0;i<=8;i++){
+		for(j=0;j<=8;j++){
+			if(i==8){
+				cout<<"*";
+			}
+			else{
+				if(j==0){
+					cout<<"*";
+				}
+				else{
+					cout<<" ";
+				}
+			}
+	}cout<<"\n";
+	}
+	cout<<"\n";
+return 0;
+
+}
